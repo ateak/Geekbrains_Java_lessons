47956be8fdb9cf78bfb916b7517dfeae498beb42
ateak <ekaterina_timoshkina@inbox.ru>
2023-03-17 12:55:13 +0300

    finally с try/catch

--- lesson3/src/main/java/com/geekbrains/lesson3/exceptions/MainApp.java
+++ lesson3/src/main/java/com/geekbrains/lesson3/exceptions/MainApp.java
@@ -46,7 +46,9 @@ public class MainApp {
             e.printStackTrace();
         } finally {
             try {
-                out.close();
+                if (out != null) {
+                    out.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
