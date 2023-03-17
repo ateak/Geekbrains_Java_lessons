fae6f0b3981c535e4f65b5fce91bcba624895fe2
ateak <ekaterina_timoshkina@inbox.ru>
2023-03-17 12:31:36 +0300

    finally

--- lesson3/src/main/java/com/geekbrains/lesson3/exceptions/MainApp.java
+++ lesson3/src/main/java/com/geekbrains/lesson3/exceptions/MainApp.java
@@ -1,29 +1,71 @@
 package com.geekbrains.lesson3.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class MainApp {
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-       try {
-           serverSocket = new ServerSocket(8189);
+//        ServerSocket serverSocket = null;
+//       try {
+//           serverSocket = new ServerSocket(8189);
+//
+//       } catch (IOException e) {
+//           e.printStackTrace();
+//           try {
+//               serverSocket.close();
+//           } catch (IOException e1) {
+//               e1.printStackTrace();
+//           }
+//       }
 
-       } catch (IOException e) {
-           e.printStackTrace();
-           try {
-               serverSocket.close();
-           } catch (IOException e1) {
-               e1.printStackTrace();
-           }
+        //  конструктор класса предупреждает что здесь может возни кнуть исключение
+        //поэтому если его не обработать, код не запустится
+//        int x = 10 / 0;
+//        try {
+//            FileOutputStream out = new FileOutputStream("1.txt");
+//        } catch (FileNotFoundException e) {
+//           e.printStackTrace();
+//        }
+
+        //ReportMaker.makePDFReport("1.txt", "gfdhgjfjhgm");
+
+//        try {
+//            int x = 10 / 0;
+//        } catch (NullPointerException e) {
+//            System.out.println("Exception");
+//        } finally {
+//            System.out.println(1);
+//        }
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream("1.txt");
+            out.write(1);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static int doSomething() throws FileNotFoundException {
+       try {
+           return 1;
+       } finally {
+           return 2;
        }
-        System.out.println(3);
-        //a();
     }
-    public static void a() {
-        b();
-    }
-    public static void b() {
-        int x = 10 / 0;
+
+    public static int sqrt(int n) {
+        if (n < 0) {
+            throw new ArithmeticException("Невозможно взять корень из отрицательного числа");
+        }
+        return n / 2;
     }
 }
