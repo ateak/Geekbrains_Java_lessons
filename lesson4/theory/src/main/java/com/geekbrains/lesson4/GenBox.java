81eebb2c47e97230b2008a9786962c615b67166b
ateak <ekaterina_timoshkina@inbox.ru>
2023-03-21 17:33:49 +0300

    больше про дженерики: обобщенные методы, интерфейсы

--- lesson4/theory/src/main/java/com/geekbrains/lesson4/GenBox.java
+++ lesson4/theory/src/main/java/com/geekbrains/lesson4/GenBox.java
@@ -3,6 +3,9 @@ package com.geekbrains.lesson4;
 public class GenBox<T> {
     private T obj;
 
+    // так делать нельзя
+    // static T staticField = ...
+
     public T getObj() {
         return obj;
     }
@@ -14,4 +17,17 @@ public class GenBox<T> {
     public GenBox(T obj) {
         this.obj = obj;
     }
+
+    // создавать объект обобщеного типа или создавпть массив этого типа в методе нельзя
+//    public void doSomething() {
+//        T object = new T();
+//        T[] arr = new T[10];
+//    }
+
+        // так делать можно
+//    public void doSomething(T[] arr) {
+//
+//    }
+
+
 }
